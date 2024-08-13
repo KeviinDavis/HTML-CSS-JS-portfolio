71bd11bcdf7886365b364731a191f5914de0929a
KeviinDavis <daviskevin1@hotmail.com>
2024-08-13 18:44:51 -0400

    Basic Skeleton

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classlist.toggle("open");
+    icon.classlist.toggle("open"); 
+}
+*/
+
+/* GP4 Solution*/
+
+function toggleMenu() {
+    var menuLinks = document.querySelector(".menu-links");
+    menuLinks.classList.toggle("open");
+}
