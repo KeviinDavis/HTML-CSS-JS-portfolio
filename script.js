7a3135738c6190c7300b30dfe2001fd7fb1edd10
KeviinDavis <daviskevin1@hotmail.com>
2024-10-05 16:18:01 -0400

    almost complete

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-/*
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    menu.classlist.toggle("open");
-    icon.classlist.toggle("open"); 
+    menu.classList.toggle("open");
+    icon.classList.toggle("open"); 
 }
-*/
 
-/* GP4 Solution*/
+// /* GP4 Solution*/
 
-function toggleMenu() {
-    var menuLinks = document.querySelector(".menu-links");
-    menuLinks.classList.toggle("open");
-}
+// function toggleMenu() {
+//     var menuLinks = document.querySelector(".menu-links");
+//     menuLinks.classList.toggle("open");
+// }
